fef81d7acf498c803c63e754b5e4f00872edd679
Mark Knell <markkmail-propter@yahoo.com>
2012-09-09 11:24:34 -0700

    First nuspec.

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-Stile is a package of syntax helpers and pattern templates for C#.  It enables a certain style of terser and more English-like code.  Its name is also a bit of a pun: a stile is a feature of a fence designed to let humans pass while still being a barrier to livestock.  (The best known example, probably, being the turnstile.)  
+Stile is a package of helpers and pattern templates for a certain C# idiomatic style.  It enables terser and more English-like code.  Its name is also a bit of a pun: a stile is a feature of a fence designed to let humans pass while still being a barrier to livestock.  (The best known example, probably, being the turnstile.)  
 
 Add some Stile to your code and slip on through to the other side. 
--- source/Stile/Properties/AssemblyInfo.cs
+++ source/Stile/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Syntax helpers and pattern templates for a certain C# idiomatic style.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mark Knell")]
 [assembly: AssemblyProduct("Stile")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2010-2012 Mark Knell")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
