418e96cbef63b0151d9eadeb603c26ab6906812c
Mark Knell <mknell@epiqsystems.com>
2013-03-08 17:52:15 -0800

    Separated EqualTo code.

---
+++ source/Stile/Prototypes/Specifications/Builders/OfExpectations/Is/EqualTo.cs
@@ -0,0 +1,80 @@
+#region License info...
+// Stile for .NET, Copyright 2011-2013 by Mark Knell
+// Licensed under the MIT License found at the top directory of the Stile project on GitHub
+#endregion
+
+#region using...
+using System;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+using Stile.Patterns.Behavioral.Validation;
+using Stile.Prototypes.Specifications.SemanticModel.Expectations;
+using Stile.Prototypes.Specifications.SemanticModel.Specifications;
+using Stile.Prototypes.Specifications.SemanticModel.Visitors;
+using Stile.Readability;
+#endregion
+
+namespace Stile.Prototypes.Specifications.Builders.OfExpectations.Is
+{
+	public interface IEqualToState<out TSpecification, TSubject, TResult> :
+		IExpectationTerm<IIsState<TSpecification, TSubject, TResult>>
+		where TSpecification : class, IChainableSpecification
+	{
+		Lazy<string> Description { get; }
+		TResult Expected { get; }
+		Expression<Predicate<TResult>> Expression { get; set; }
+		TSpecification Build();
+	}
+
+	public static class EqualTo
+	{
+		public static TSpecification Make<TSpecification, TSubject, TResult>(
+			[NotNull] Expression<Predicate<TResult>> expression,
+			TResult expected,
+			[NotNull] IIsState<TSpecification, TSubject, TResult> prior)
+			where TSpecification : class, IChainableSpecification
+		{
+			var equalTo = new EqualTo<TSpecification, TSubject, TResult>(expression, prior, expected);
+			return equalTo.Build();
+		}
+	}
+
+	public class EqualTo<TSpecification, TSubject, TResult> :
+		ExpectationTerm<IIsState<TSpecification, TSubject, TResult>>,
+		IEqualToState<TSpecification, TSubject, TResult>
+		where TSpecification : class, IChainableSpecification
+	{
+		public EqualTo([NotNull] Expression<Predicate<TResult>> expression,
+			[NotNull] IIsState<TSpecification, TSubject, TResult> prior,
+			TResult expected)
+			: base(prior)
+		{
+			Expression = expression.ValidateArgumentIsNotNull();
+			Expected = expected;
+			Description = Expected.ToLazyDebugString();
+		}
+
+		public Lazy<string> Description { get; private set; }
+		public TResult Expected { get; private set; }
+		public Expression<Predicate<TResult>> Expression { get; set; }
+
+		public override void Accept(IExpectationVisitor visitor)
+		{
+			visitor.Visit3(this);
+		}
+
+		public override TData Accept<TData>(IExpectationVisitor<TData> visitor, TData data)
+		{
+			return visitor.Visit3(this, data);
+		}
+
+		public TSpecification Build()
+		{
+			Expectation<TSubject, TResult> expectation = Expectation<TSubject>.From(Expression,
+				Prior.Negated,
+				Prior.BuilderState.Instrument,
+				this);
+			return Prior.BuilderState.Make(expectation);
+		}
+	}
+}
--- source/Stile/Prototypes/Specifications/Builders/OfExpectations/Is/IsExtensions.cs
+++ source/Stile/Prototypes/Specifications/Builders/OfExpectations/Is/IsExtensions.cs
@@ -4,72 +4,20 @@
 #endregion
 
 #region using...
-using System;
-using System.Linq.Expressions;
-using JetBrains.Annotations;
-using Stile.Prototypes.Specifications.SemanticModel.Expectations;
+using System.Diagnostics.Contracts;
 using Stile.Prototypes.Specifications.SemanticModel.Specifications;
-using Stile.Prototypes.Specifications.SemanticModel.Visitors;
-using Stile.Readability;
 #endregion
 
 namespace Stile.Prototypes.Specifications.Builders.OfExpectations.Is
 {
 	public static class IsExtensions
 	{
-		[System.Diagnostics.Contracts.Pure]
+		[Pure]
 		public static TSpecification EqualTo<TSpecification, TSubject, TResult>(
 			this IIs<TSpecification, TSubject, TResult> builder, TResult result)
 			where TSpecification : class, ISpecification<TSubject, TResult>, IChainableSpecification
 		{
-			return Make(x => x.Equals(result), builder.Xray, result);
-		}
-
-		private static TSpecification Make<TSpecification, TSubject, TResult>(
-			Expression<Predicate<TResult>> expression,
-			IIsState<TSpecification, TSubject, TResult> state,
-			TResult expected) where TSpecification : class, ISpecification<TSubject, TResult>, IChainableSpecification
-		{
-			var equalTo = new EqualTo<TSpecification, TSubject, TResult>(state, expected);
-			Expectation<TSubject, TResult> expectation = Expectation<TSubject>.From(expression,
-				state.Negated,
-				state.BuilderState.Instrument,
-				equalTo);
-			return state.BuilderState.Make(expectation);
-		}
-	}
-
-	public interface IEqualToState<out TSpecification, TSubject, TResult> :
-		IExpectationTerm<IIsState<TSpecification, TSubject, TResult>>
-		where TSpecification : class, IChainableSpecification
-	{
-		Lazy<string> Description { get; }
-		TResult Expected { get; }
-	}
-
-	public class EqualTo<TSpecification, TSubject, TResult> :
-		ExpectationTerm<IIsState<TSpecification, TSubject, TResult>>,
-		IEqualToState<TSpecification, TSubject, TResult>
-		where TSpecification : class, IChainableSpecification
-	{
-		public EqualTo([NotNull] IIsState<TSpecification, TSubject, TResult> prior, TResult expected)
-			: base(prior)
-		{
-			Expected = expected;
-			Description = Expected.ToLazyDebugString();
-		}
-
-		public Lazy<string> Description { get; private set; }
-		public TResult Expected { get; private set; }
-
-		public override void Accept(IExpectationVisitor visitor)
-		{
-			visitor.Visit3(this);
-		}
-
-		public override TData Accept<TData>(IExpectationVisitor<TData> visitor, TData data)
-		{
-			return visitor.Visit3(this, data);
+			return Is.EqualTo.Make(x => x.Equals(result), result, builder.Xray);
 		}
 	}
 }
